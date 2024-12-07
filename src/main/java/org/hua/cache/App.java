27e9725107f272d42ee65a16e05f67753a63b94f
Panagiotis Varitis <panosvaritis@icloud.com>
2024-12-07 21:19:24 +0200

    A test in in the list

--- src/main/java/org/hua/cache/App.java
+++ src/main/java/org/hua/cache/App.java
@@ -1,6 +1,5 @@
 
 package org.hua.cache;
-
 /**
  *
  * @author panos
@@ -13,17 +12,7 @@ public class App {
          * Bellow the actual program will be executed!!!
         */
         Cache<Integer, String> myCache = new LRUCache(10);
-        myCache.put(2021001, "Marousakis");
-        myCache.put(2021002, "Perdios");
-        myCache.put(2021003, "Moraitis");
-        myCache.put(2021004, "Dimitrakopoulos");
-        
-        myCache.print();
-
-
-//        myCache.get(2021000);//will return the value of the key
-//        myCache.get(2021003);//will return thr value
-//        myCache.get(2021); /// Does not exist exception
+               
 
     }
 }
