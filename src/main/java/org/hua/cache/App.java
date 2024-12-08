3e484e0df2bd80da1bdbba9f752f279b644e34c6
Panagiotis Varitis <panosvaritis@icloud.com>
2024-12-08 14:43:09 +0200

    Simple user interface

--- src/main/java/org/hua/cache/App.java
+++ src/main/java/org/hua/cache/App.java
@@ -12,20 +12,35 @@ public class App {
         /**
          * Bellow the actual program will be executed!!!
         */
-//        Cache<Integer, String> myCache = new LRUCache(10);
-        LRUCache<Integer, String> myCache = new LRUCache(10);
-
-        
-        myCache.put(1, "Panos");
-        myCache.put(1, "Dimiris");
-        myCache.put(1, "Marios");
-        myCache.put(1, "Maria");
-        myCache.put(1, "Thanasis");
-        myCache.put(1, "George");
-        myCache.put(1, "Georgia");
-        myCache.put(1, "Panagiota");
-        myCache.put(1, "Panagiotis");
-
-        
+        Cache<Integer, String> myCache = new LRUCache(3);
+//        LRUCache<Integer, String> myCache = new LRUCache(10);
+    
+    myCache.put(1, "One");
+    myCache.put(2, "Two");
+    myCache.put(3, "Three");
+    
+    
+    System.out.println ("Get key 1: "+myCache.get(1));
+    System.out.println ("Get key 2: "+myCache.get(2));
+    System.out.println ("Get key 3: "+myCache.get(3));
+    
+    myCache.put(4, "Four");
+    
+    System.out.println ("Get key 1 after addind 4: "+myCache.get(1));
+    System.out.println ("Get key 2 after adding 4: "+myCache.get(2));
+    System.out.println ("Get key 3 after adding 4: "+myCache.get(3));
+    System.out.println ("Get key 4: "+myCache.get(4));
+    
+    
+    myCache.put(5, "Five");
+    
+    System.out.println ("Get key 2 after addind 5: "+myCache.get(2));
+    System.out.println ("Get key 3 after adding 5: "+myCache.get(3));
+    System.out.println ("Get key 4 after adding 5: "+myCache.get(4));
+    System.out.println ("Get key 5: "+myCache.get(5));
+    
+            
+    
+    
     }
 }
