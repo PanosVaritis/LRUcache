b736960d98bc2b522b00559cda5ed42ad7180cd0
Panagiotis Varitis <panosvaritis@icloud.com>
2024-12-10 10:21:12 +0200

    One more test added to the cache

--- src/main/java/org/hua/cache/App.java
+++ src/main/java/org/hua/cache/App.java
@@ -9,38 +9,22 @@ public class App {
 
     public static void main(String[] args) {
         
-        /**
-         * Bellow the actual program will be executed!!!
-        */
         Cache<Integer, String> myCache = new LRUCache(3);
-//        LRUCache<Integer, String> myCache = new LRUCache(10);
-    
-    myCache.put(1, "One");
-    myCache.put(2, "Two");
-    myCache.put(3, "Three");
-    
-    
-    System.out.println ("Get key 1: "+myCache.get(1));
-    System.out.println ("Get key 2: "+myCache.get(2));
-    System.out.println ("Get key 3: "+myCache.get(3));
-    
-    myCache.put(4, "Four");
-    
-    System.out.println ("Get key 1 after addind 4: "+myCache.get(1));
-    System.out.println ("Get key 2 after adding 4: "+myCache.get(2));
-    System.out.println ("Get key 3 after adding 4: "+myCache.get(3));
-    System.out.println ("Get key 4: "+myCache.get(4));
-    
-    
-    myCache.put(5, "Five");
-    
-    System.out.println ("Get key 2 after addind 5: "+myCache.get(2));
-    System.out.println ("Get key 3 after adding 5: "+myCache.get(3));
-    System.out.println ("Get key 4 after adding 5: "+myCache.get(4));
-    System.out.println ("Get key 5: "+myCache.get(5));
-    
-            
-    
-    
+
+        myCache.put(1, "One");
+        myCache.put(2, "Two");
+        myCache.put(3, "Three");
+
+
+        System.out.println ("Key with number 1 corresponds to value: "+myCache.get(1));
+        System.out.println ("Key with number 5 corresponds to value: "+myCache.get(5));
+        System.out.println ("Key with number 3 corresponds to value: "+myCache.get(3));
+
+
+        myCache.put (2, "Hello");
+        myCache.put(4, "Four");
+
+        System.out.println ("Key with number 1 corresponds to value: "+myCache.get(1));
+
     }
 }
--- src/test/java/org/hua/cache/LRUCacheTest.java
+++ src/test/java/org/hua/cache/LRUCacheTest.java
@@ -103,4 +103,29 @@ public class LRUCacheTest {
         assertNull(cache.get(4));
     }
     
+    @Test
+    public void cacheTest5(){
+        
+        for (int i = 0;i < 100;i++){
+            cache.put(i,"Text");
+        }
+        
+        for (int i = 0;i < 97;i++){
+            assertNull (cache.get(i));
+        }
+        
+        assertEquals("Text", cache.get(99));
+        assertEquals("Text", cache.get(98));
+        assertEquals("Text", cache.get(97));
+        
+        cache.put(1, "Panos");
+        assertNull(cache.get(99));
+        cache.put(2, "Marios");
+        assertNull (cache.get(88));
+        cache.put(3, "Dimitris");
+        assertNull (cache.get(97));
+        cache.put(4, "Marios");
+        assertNull (cache.get(1));
+    }
+    
 }
